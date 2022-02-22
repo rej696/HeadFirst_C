32487c0460ea6edc8d77f5917845b17c34f50a4a
rej696 <55083730+rej696@users.noreply.github.com>
2022-02-22 08:47:11 +0000

    drinks

---
+++ Tutorials/drinks.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+enum drink { MUDSLIDE, FUZZY_NAVEL, MONKEY_GLAND, ZOMBIE };
+
+double price(enum drink d) {
+  switch (d) {
+  case MUDSLIDE:
+    return 6.79;
+  case FUZZY_NAVEL:
+    return 5.31;
+  case MONKEY_GLAND:
+    return 4.82;
+  case ZOMBIE:
+    return 5.89;
+  }
+  return 0;
+}
+
+double total(int args, ...) {
+  double total = 0;
+  va_list ap;
+  va_start(ap, args);
+  for (int i = 0; i < args; i++) {
+    total += price(va_arg(ap, enum drink));
+  }
+  va_end(ap);
+  return total;
+}
+
+int main() {
+  printf("Price is %.2f\n", total(3, MONKEY_GLAND, MUDSLIDE, FUZZY_NAVEL));
+  printf("Price is %.2f\n", total(4, MUDSLIDE, MUDSLIDE, FUZZY_NAVEL, ZOMBIE));
+  printf("Price is %.2f\n", total(2, MONKEY_GLAND, ZOMBIE));
+  return 0;
+}
